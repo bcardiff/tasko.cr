4c5fc8e4e701af1ac76b8fe4790dada3874787ee
Brian J. Cardiff <bcardiff@gmail.com>
2021-02-27 20:59:21 -0300

    Show arrows from completed tasks in green
    Escape " in labels

--- src/tasko/dot.cr
+++ src/tasko/dot.cr
@@ -1,17 +1,24 @@
 module Tasko
   def self.generate_dot(io : IO, application : Application)
     tasks = application.engine.stats
+    completed = Set(Key).new
 
     io.puts "digraph Tasks {"
     tasks.each do |t|
-      io.puts %Q(  "#{t.descriptor.key}" [label="#{t.descriptor.name} #{t.descriptor.serialized_data}" color="#{t.completed ? "green" : "black"}"])
+      if t.completed
+        completed << t.descriptor.key
+        extra = %Q( color=green)
+      end
+      label = "#{t.descriptor.name} #{t.descriptor.serialized_data}"
+      io.puts %Q(  "#{t.descriptor.key}" [label=#{label.inspect}#{extra}])
     end
     tasks.each do |t|
       if parent = t.initiated_by
         io.puts %Q(  "#{parent}" -> "#{t.descriptor.key}" [style=dashed])
       end
       application.engine.tasks_dependencies(t.descriptor.key).each do |dep|
-        io.puts %Q(  "#{dep}" -> "#{t.descriptor.key}")
+        extra = %Q( [color=green]) if completed.includes?(dep)
+        io.puts %Q(  "#{dep}" -> "#{t.descriptor.key}"#{extra})
       end
     end
     io.puts "}"
