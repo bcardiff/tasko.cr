d79b1a3e3d52a16691915974837b8fb62e567894
Brian J. Cardiff <bcardiff@gmail.com>
2021-03-01 18:27:58 -0300

    Move data macro inside defining class

--- src/tasko/kv_store.cr
+++ src/tasko/kv_store.cr
@@ -18,30 +18,30 @@ end
 
 module Tasko
   macro store(class_name)
-    macro data(name_and_type, *, indexed_by = nil)
-      \{% store_prefix = @type.name %}
-      \{% if name_and_type.is_a?(TypeDeclaration) %}
-        \{% name = name_and_type.var.id %}
-        \{% return_type = name_and_type.type %}
-        \{% if indexed_by %}
-          def \{{name}}
-            ::Tasko::KVStore::IndexedData(\{{indexed_by}}, \{{return_type}}).new(@store, \{{store_prefix.stringify}}, \{{name.stringify}})
-          end
-        \{% else %}
-          def \{{name}}=(value : \{{return_type}})
-            @store.save("\{{store_prefix}}:\{{name}}", value)
-          end
-
-          def \{{name}} : \{{return_type}}
-            @store.load("\{{store_prefix}}:\{{name}}", as: \{{return_type}})
-          end
-        \{% end %}
-      \{% else %}
-        \{% raise "data should be used with type declaration: `data name : Type, [index]`" %}
-      \{% end %}
-    end
-
     class {{class_name}}
+      macro data(name_and_type, *, indexed_by = nil)
+        \{% store_prefix = @type.name %}
+        \{% if name_and_type.is_a?(TypeDeclaration) %}
+          \{% name = name_and_type.var.id %}
+          \{% return_type = name_and_type.type %}
+          \{% if indexed_by %}
+            def \{{name}}
+              ::Tasko::KVStore::IndexedData(\{{indexed_by}}, \{{return_type}}).new(@store, \{{store_prefix.stringify}}, \{{name.stringify}})
+            end
+          \{% else %}
+            def \{{name}}=(value : \{{return_type}})
+              @store.save("\{{store_prefix}}:\{{name}}", value)
+            end
+
+            def \{{name}} : \{{return_type}}
+              @store.load("\{{store_prefix}}:\{{name}}", as: \{{return_type}})
+            end
+          \{% end %}
+        \{% else %}
+          \{% raise "data should be used with type declaration: `data name : Type, [index]`" %}
+        \{% end %}
+      end
+
       @store : ::Tasko::KVStore
 
       def initialize(engine : ::Tasko::Engine)
